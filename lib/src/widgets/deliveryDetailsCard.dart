36af4bebc33f1ffd5446800b34f00d7719c6ac5d
Chirag Modh <91516739+22chiragmodh@users.noreply.github.com>
2025-01-10 09:54:01 +0530

    add call launcher

--- lib/src/widgets/deliveryDetailsCard.dart
+++ lib/src/widgets/deliveryDetailsCard.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DeliveryDetailsCard extends StatelessWidget {
   final String location;
   final String phone;
   DeliveryDetailsCard({super.key, required this.location, required this.phone});
+
+  void _launchCaller(String Phone) async {
+    final url = "tel:$Phone";
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw "Could not launch $url";
+    }
+  }
+
+  Future<void> _makePhoneCall(String phoneNumber) async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launchUrl(launchUri);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,13 +83,23 @@ class DeliveryDetailsCard extends StatelessWidget {
                               fontFamily: 'SF Pro',
                               fontWeight: FontWeight.w400,
                             )),
-                        Text(phone,
-                            style: TextStyle(
-                              color: Color(0xFF222222),
-                              fontSize: 16,
-                              fontFamily: 'SF Pro',
-                              fontWeight: FontWeight.w400,
-                            )),
+                        Row(
+                          children: [
+                            IconButton(
+                              onPressed: () => _makePhoneCall(phone),
+                              icon: Icon(Icons.phone,
+                                  size: 20,
+                                  color: Color.fromARGB(255, 19, 113, 189)),
+                            ),
+                            Text(phone,
+                                style: TextStyle(
+                                  color: Color(0xFF222222),
+                                  fontSize: 16,
+                                  fontFamily: 'SF Pro',
+                                  fontWeight: FontWeight.w400,
+                                )),
+                          ],
+                        ),
                       ],
                     )
                   ],
